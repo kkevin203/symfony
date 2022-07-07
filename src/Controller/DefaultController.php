c47b640bf24d2089bc2027f895295a6019bbf7f6
Kevin.Bonnet <103254324+kkevin203@users.noreply.github.com>
2022-07-07 21:59:27 +0200

    création projet synfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/",name="index")
+     * 
+     * @return Response
+     */
+    
+    public function index(): Response
+    {
+        return new Response('Hello world!');
+    }
+    public function contact(): Response
+    {
+      return $this->render('default/contact.html.twig', [
+            'title' => 'Ma page de contact',
+        ]);
+    }
+}
